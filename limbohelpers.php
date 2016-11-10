b30a0bc2e3c38c7e2f0ba2232e003653f1255639
syni679 <syni679@gmail.com>
2016-11-10 00:46:53 -0500

    Add files via upload

--- limbohelpers.php
+++ limbohelpers.php
@@ -2,19 +2,15 @@
 # Helpers.php 
 # Authors: Kai Wong, Wendy Ni, Jae Kyoung Lee (LJ)
 # Date: 11/02/2016
-
 $debug = true;
-
+# Shows the records in presidents
 function show_records($dbc) {
 		# Connect to MySQL server and the database
 		require( 'includes/connect_limbo_db.php' ) ;
-
 		# Create a query to get the number, first name, and last name sorted by number in descending order
 		$query = 'SELECT number, fname, lname FROM stuff ORDER BY number DESC' ;
-
 		# Execute the query
 		$results = mysqli_query( $dbc , $query ) ;
-
 		# Show results
 		if( $results )
 		{
@@ -27,7 +23,6 @@ function show_records($dbc) {
 		  echo '<TH>First Name</TH>';
 		  echo '<TH>Last Name</TH>';
 		  echo '</TR>';
-
 		  # For each row result, generate a table row
 		  while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
 		  {
@@ -36,10 +31,8 @@ function show_records($dbc) {
 			echo '<TD>' . $row['finder_name'] . '</TD>' ;
 			echo '</TR>' ;
 		  }
-
 		  # End the table
 		  echo '</TABLE>';
-
 		  # Free up the results in memory
 		  mysqli_free_result( $results ) ;
 		}
@@ -48,7 +41,6 @@ function show_records($dbc) {
 		  # If we get here, something has gone wrong
 		  echo '<p>' . mysqli_error( $dbc ) . '</p>'  ;
 		}
-
 		# Close the connection
 		mysqli_close( $dbc ) ;
 }
@@ -56,13 +48,10 @@ function show_records($dbc) {
 function show_found_records($dbc) {
 		# Connect to MySQL server and the database
 		require( 'includes/connect_limbo_db.php' ) ;
-
 		# Create a query to get the id and last name sorted by id in ascending order
-		$query = 'SELECT finder_name, phone_number, email, item_name, description, location_name, found_date FROM foundstuff' ;
-
+		$query = 'SELECT id, finder_name, phone_number, email, item_name, description, location_name, found_date, status FROM foundstuff' ;
 		# Execute the query
 		$results = mysqli_query( $dbc , $query ) ;
-
 		# Show results
 		if( $results )
 		{
@@ -71,6 +60,7 @@ function show_found_records($dbc) {
 		  echo '<H1>Found Stuff</H1>' ;
 		  echo '<TABLE border=1 style = "font-family:courier;">';
 		  echo '<TR>';
+		  echo '<TH>Id</TH>';
 		  echo '<TH>Finder Name</TH>';
 		  echo '<TH>Phone Number</TH>';
 		  echo '<TH>Email</TH>';
@@ -78,8 +68,8 @@ function show_found_records($dbc) {
 		  echo '<TH>Description of item</TH>';
 		  echo '<TH>Location</TH>';
 		  echo '<TH>Date</TH>';
+		  echo '<TH>Status</TH>';
 		  echo '</TR>';
-
 		  # For each row result, generate a table row
 		  while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
 		  {
@@ -90,6 +80,7 @@ function show_found_records($dbc) {
 			echo '<TD>' . $row['description'] . '</TD>' ;
 			echo '</TR>' ; */
 			echo '<TR>' ;
+			echo '<TD>' . $row['id'] . '</TD>' ;
 			echo '<TD>' . $row['finder_name'] . '</TD>' ;
 			echo '<TD>' . $row['phone_number'] . '</TD>' ;
 			echo '<TD>' . $row['email'] . '</TD>' ;
@@ -97,12 +88,11 @@ function show_found_records($dbc) {
 			echo '<TD>' . $row['description'] . '</TD>' ;
 			echo '<TD>' . $row['location_name'] . '</TD>' ;
 			echo '<TD>' . $row['found_date'] . '</TD>' ;
+			echo '<TD>' . $row['status'] . '</TD>' ;
 			echo '</TR>' ;
 		  }
-
 		  # End the table
 		  echo '</TABLE>';
-
 		  # Free up the results in memory
 		  mysqli_free_result( $results ) ;
 		}
@@ -111,76 +101,16 @@ function show_found_records($dbc) {
 		  # If we get here, something has gone wrong
 		  echo '<p>' . mysqli_error( $dbc ) . '</p>'  ;
 		}
-
 		# Close the connection
 		mysqli_close( $dbc ) ;
 }
-
-function show_lost_records($dbc) {
-		# Connect to MySQL server and the database
-		require( 'includes/connect_limbo_db.php' ) ;
-
-		# Create a query to get the id and last name sorted by id in ascending order
-		$query = 'SELECT item_name, location_name, lost_date FROM loststuff';
-
-		# Execute the query
-		$results = mysqli_query( $dbc , $query ) ;
-
-		# Show results
-		if( $results )
-		{
-		  # But...wait until we know the query succeeded before
-		  # starting the table.
-		  echo '<H1>Lost Stuff</H1>' ;
-		  echo '<TABLE border=1 style = "font-family:courier;">';
-		  echo '<TR>';
-		  echo '<TH>Name of Item</TH>';
-		  echo '<TH>Location</TH>';
-		  echo '<TH>Date</TH>';
-		  echo '</TR>';
-
-		  # For each row result, generate a table row
-		  while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
-		  {
-			/* //Creates an anchor link to display more information about the president
-			$alink = '<a href = found.php?id=' . $row['id'] . '>' . $row['id'] . '</a>';
-			echo '<TR>' ;
-			echo '<TD align = right>' . $alink . '</TD>';
-			echo '<TD>' . $row['description'] . '</TD>' ;
-			echo '</TR>' ; */
-			echo '<TR>' ;
-			echo '<TD>' . $row['item_name'] . '</TD>' ;
-			echo '<TD>' . $row['location_name'] . '</TD>' ;
-			echo '<TD>' . $row['lost_date'] . '</TD>' ;
-			echo '</TR>' ;
-		  }
-
-		  # End the table
-		  echo '</TABLE>';
-
-		  # Free up the results in memory
-		  mysqli_free_result( $results ) ;
-		}
-		else
-		{
-		  # If we get here, something has gone wrong
-		  echo '<p>' . mysqli_error( $dbc ) . '</p>'  ;
-		}
-
-		# Close the connection
-		mysqli_close( $dbc ) ;
-}
-
 function show_record($dbc, $id) {
 		# Connect to MySQL server and the database
 		require( 'includes/connect_db.php' ) ;
-
 		# Create a query to get the number, first name, and last name sorted by number in descending order
 		$query = 'SELECT finder_name FROM stuff WHERE id = ' . $id;
-
 		# Execute the query
 		$results = mysqli_query( $dbc , $query ) ;
-
 		# Show results
 		if( $results )
 		{
@@ -193,7 +123,6 @@ function show_record($dbc, $id) {
 		  echo '<TH>Last Name</TH>';
 		  echo '<TH>First Name</TH>';
 		  echo '</TR>'; */
-
 		  # For each row result, generate a table row
 		  while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
 		  {
@@ -203,10 +132,8 @@ function show_record($dbc, $id) {
 			echo '<TD>' . $row['found_date'] . '</TD>' ;
 			echo '</TR>' ;
 		  }
-
 		  # End the table
 		  echo '</TABLE>';
-
 		  # Free up the results in memory
 		  mysqli_free_result( $results ) ;
 		}
@@ -215,48 +142,37 @@ function show_record($dbc, $id) {
 		  # If we get here, something has gone wrong
 		  echo '<p>' . mysqli_error( $dbc ) . '</p>'  ;
 		}
-
 		# Close the connection
 		mysqli_close( $dbc ) ;
 }
-
-# Inserts a record into the presidents table with number, first name, and last name
+# Inserts a record into the found table with number, first name, and last name
 function insert_record_foundstuff($dbc, $findername, $phone, $email, $itemname, $description, $location, $date) {
-  $query = 'INSERT INTO foundstuff(finder_name, phone_number, email, item_name, description, location_name, found_date, status) VALUES ("' . $findername . '" , "' . $phone . '" , "' . $email . '" , "' . $description . '" , "' . $itemname . '", "' . $location . '", "' . $date . '" , "Found")' ;
+  $query = 'INSERT INTO foundstuff(finder_name, phone_number, email, item_name, description, location_name, found_date) VALUES ("' . $findername . '" , "' . $phone . '" , "' . $email . '" , "' . $description . '" , "' . $itemname . '", "' . $location . '", "' . $date . '" )' ;
   show_query($query);
-
   $results = mysqli_query($dbc,$query) ;
   check_results($results) ;
-
   return $results ;
 }
-
-function insert_record_loststuff($dbc, $ownername, $phone, $email, $itemname, $description, $location, $date) {
-  $query = 'INSERT INTO loststuff(owner_name, phone_number, email, item_name, description, location_name, lost_date, status) VALUES ("' . $ownername . '" , "' . $phone . '" , "' . $email . '" , "' . $description . '" , "' . $itemname . '", "' . $location . '", "' . $date . '" , "Lost")' ;
+# Update status of item into the found table using id
+function update_status_foundstuff($dbc, $id, $status) {
+  $query = "UPDATE foundstuff SET status='" .$status."' WHERE id='".$id."'";
   show_query($query);
-
   $results = mysqli_query($dbc,$query) ;
   check_results($results) ;
-
   return $results ;
 }
-
 # Shows the query as a debugging aid
 function show_query($query) {
   global $debug;
-
   if($debug)
     echo "<p>Query = $query</p>" ;
 }
-
 # Checks the query results as a debugging aid
 function check_results($results) {
   global $dbc;
-
   if($results != true)
     echo '<p>SQL ERROR = ' . mysqli_error( $dbc ) . '</p>'  ;
 }
-
 #Created function that validates a number
 function valid_number($num) {
 	if (empty($num) || !is_numeric($num))
@@ -268,7 +184,6 @@ function valid_number($num) {
 	}
 	return true;
 }
-
 #Created function that validates name input
 function valid_name($name) {
 	if (empty($name))
@@ -276,6 +191,4 @@ function valid_name($name) {
 	else 
 		return true;
 }
-
-
 ?>
