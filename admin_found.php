d91e2c6fd5df8c87ebfac98305d30f9a8a8fa031
syni679 <syni679@gmail.com>
2016-11-11 00:32:00 -0500

    Add files via upload

--- admin_found.php
+++ admin_found.php
@@ -11,81 +11,85 @@ Edited by Kai Wong, Wendy Ni, Jae Kyoung Lee (LJ)
 -->
 <!DOCTYPE html>
 <html>
-
-<head>
-	<meta charset="utf-8">
-	<link href="home.css" type="text/css" rel="stylesheet">
-	<link href="admin.css" type="text/css" rel="stylesheet">
-	<title>Manage Found</title>
-</head>
-<body>
-<?php
-		# Connect to MySQL server and the database
-		require( 'includes/connect_limbo_db.php' ) ;
-		# Includes these helper functions
-		require( 'includes/limbohelpers.php' ) ;
-		
-		# Check to make sure it is the first time user is visiting the page
-		if ($_SERVER['REQUEST_METHOD'] == 'GET'){
-			$id = '';
-			$status = '';
-		}
-		# Check to make sure the form method is post
-		if ($_SERVER[ 'REQUEST_METHOD' ] == 'POST') {
-			$id = $_POST['f_id'];
-			$status = $_POST['f_status'];
-			
-			#Checks to see if name input is valid	
-			if (empty($id)&&empty($status)) 
-				echo 'error';
-			else{
-				$id = trim($id);
-				$status = trim($status);
-				$result = update_status_foundstuff($dbc, $id, $status);
-				echo "Success! Thanks" ; 
-			}
-		}
-
-		# Close the connection
-		mysqli_close( $dbc ) ;
-		?>
-		<?php if($_SERVER['REQUEST_METHOD'] == 'GET'){
+	<head>
+		<meta charset="utf-8">
+		<link href="home.css" type="text/css" rel="stylesheet">
+		<link href="admin.css" type="text/css" rel="stylesheet">
+		<title>Manage Found</title>
+	</head>
+	<body>
+			<!--Used to change status of items-->
+		<?php
 			# Connect to MySQL server and the database
-		require( 'includes/connect_limbo_db.php' ) ;
-			if(isset($_GET['id']))
-				admin_found_delete($dbc, $_GET['id']);
+			require( 'includes/connect_limbo_db.php' ) ;
+			# Includes these helper functions
+			require( 'includes/limbohelpers.php' ) ;
+			# Check to make sure it is the first time user is visiting the page
+			if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+				$id = '';
+				$status = '';
+			}
+			# Check to make sure the form method is post
+			if ($_SERVER[ 'REQUEST_METHOD' ] == 'POST') {
+				$id = $_POST['f_id'];
+				$status = $_POST['f_status'];
+				#Checks to see if inputs are entered	
+				if (empty($id)&&empty($status)) 
+					echo 'Please enter both fields to continue';
+				else{
+					$id = trim($id);
+					$status = trim($status);
+					$result = update_status_foundstuff($dbc, $id, $status);
+					echo "Success! Thanks" ; 
+				}
+			}
 			# Close the connection
-		mysqli_close( $dbc ) ;
-		}?>
-	<img src="maristlogo.png" id="maristlogo">
-	<br><br>
-		<!--page layout-->
-	<div style="position: relative; left: 0; top: 0;">
-			<!--menu for navigation-->
-		<ul>
-		  <li><a href="Home.html">Home</a></li>
-		  <li><a href="Lost.php">Lost</a></li>
-		  <li><a href="Found.php">Found</a></li>
-		  <li><a href="admin_login.php">Admin</a></li>
-		</ul>
-	<img src="white.jpg" height="500" width="1350" style="opacity: 0.8; position: relative; top: -136px; left: 100px;"/>
-	</div>
-		<!--make updates to found-->
-	<div style="position: relative; bottom: 600px; left: 200px;"> 
-		<?php 		
-		# Show the records
-		admin_show_found_records($dbc); ?>
-		<br/>
-		<form action ="admin_found.php" method = "POST">
-			Id #*: <input type="text" name="f_id" value="<?php if (isset($_POST['f_id'])) echo $_POST['f_id'];?>")><br>
-			Status*: <input type="text" name="f_status" value="<?php if (isset($_POST['f_status'])) echo $_POST['f_status'];?>")><br>
-			<input type = "submit" >
-		</form>
-	</div>
-	<div style="position: relative; bottom: 600px; left: 200px;"> 
-		<form>
-		<button id="butlost" class="button" formaction="admin.html">Back to task page</button>
-		</form>
+			mysqli_close( $dbc ) ;
+		?>
+				<!--Used to delete items-->
+		<?php 
+			if($_SERVER['REQUEST_METHOD'] == 'GET'){
+				# Connect to MySQL server and the database
+				require( 'includes/connect_limbo_db.php' ) ;
+				# Delete item
+				if(isset($_GET['id']))
+					admin_found_delete($dbc, $_GET['id']);
+				# Close the connection
+				mysqli_close( $dbc ) ;
+			}
+		?>
+			<!--page layout-->
+		<img src="maristlogo.png" id="maristlogo">
+		<br><br>
+		<div style="position: relative; left: 0; top: 0;">
+				<!--menu for navigation-->
+			<ul>
+				<li><a href="Home.html">Home</a></li>
+				<li><a href="Lost.php">Lost</a></li>
+				<li><a href="Found.php">Found</a></li>
+				<li><a href="admin_login.php">Admin</a></li>
+			</ul>
+			<img src="white.jpg" height="500" width="1350" style="opacity: 0.8; position: relative; top: -136px; left: 100px;"/>
 		</div>
-</body>
+			<!--make updates to found-->
+		<div style="position: relative; bottom: 600px; left: 200px;"> 
+			<?php 		
+				# Show the records
+				admin_show_found_records($dbc); 
+			?>
+			<br>
+			<h1> Update Status of item: </h1>
+			<form action ="admin_found.php" method = "POST">
+				Id #*: <input type="text" name="f_id" value="<?php if (isset($_POST['f_id'])) echo $_POST['f_id'];?>")><br>
+				Status*: <input type="text" name="f_status" value="<?php if (isset($_POST['f_status'])) echo $_POST['f_status'];?>")><br>
+				<input type = "submit" >
+			</form>
+		</div>
+			<!-- allow admin to go back to task page-->
+		<div style="position: relative; bottom: 600px; left: 200px;"> 
+			<form>
+				<button id="butlost" class="button" formaction="admin.html">Back to task page</button>
+			</form>
+		</div>
+	</body>
 </html>
--- admin_lost.php
+++ admin_lost.php
@@ -11,79 +11,85 @@ Edited by Kai Wong, Wendy Ni, Jae Kyoung Lee (LJ)
 -->
 <!DOCTYPE html>
 <html>
-
-<head>
-	<meta charset="utf-8">
-	<link href="home.css" type="text/css" rel="stylesheet">
-	<link href="admin.css" type="text/css" rel="stylesheet">
-	<title>Manage Lost</title>
-</head>
-<body>
-<?php
-		# Connect to MySQL server and the database
-		require( 'includes/connect_limbo_db.php' ) ;
-		# Includes these helper functions
-		require( 'includes/limbohelpers.php' ) ;
-		# Check to make sure it is the first time user is visiting the page
-		if ($_SERVER['REQUEST_METHOD'] == 'GET'){
-			$id = '';
-			$status = '';
-		}
-		# Check to make sure the form method is post
-		if ($_SERVER[ 'REQUEST_METHOD' ] == 'POST') {
-			$id = $_POST['l_id'];
-			$status = $_POST['l_status'];
-			
-			#Checks to see if name input is valid	
-			if (empty($id)&&empty($status)) 
-				echo 'error';
-			else{
-				$id = trim($id);
-				$status = trim($status);
-				$result = update_status_loststuff($dbc, $id, $status);
-				echo "Success! Thanks" ; 
-			}
-		}
-		# Close the connection
-		mysqli_close( $dbc ) ;
-		?>
-		<?php if($_SERVER['REQUEST_METHOD'] == 'GET'){
+	<head>
+		<meta charset="utf-8">
+		<link href="home.css" type="text/css" rel="stylesheet">
+		<link href="admin.css" type="text/css" rel="stylesheet">
+		<title>Manage Lost</title>
+	</head>
+	<body>
+			<!--Used to change status of items-->
+		<?php
 			# Connect to MySQL server and the database
 			require( 'includes/connect_limbo_db.php' ) ;
-			if(isset($_GET['id']))
-				admin_lost_delete($dbc, $_GET['id']);
+			# Includes these helper functions
+			require( 'includes/limbohelpers.php' ) ;
+			# Check to make sure it is the first time user is visiting the page
+			if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+				$id = '';
+				$status = '';
+			}
+			# Check to make sure the form method is post
+			if ($_SERVER[ 'REQUEST_METHOD' ] == 'POST') {
+				$id = $_POST['l_id'];
+				$status = $_POST['l_status'];
+				#Checks to see if inputs are entered	
+				if (empty($id)&&empty($status)) 
+					echo 'Please enter both fields to continue';
+				else{
+					$id = trim($id);
+					$status = trim($status);
+					$result = update_status_loststuff($dbc, $id, $status);
+					echo "Success! Thanks" ; 
+				}
+			}
 			# Close the connection
 			mysqli_close( $dbc ) ;
-		}?>
-	<img src="maristlogo.png" id="maristlogo">
-	<br><br>
-		<!--page layout-->
-	<div style="position: relative; left: 0; top: 0;">
-			<!--menu for navigation-->
-		<ul>
-		  <li><a href="Home.html">Home</a></li>
-		  <li><a href="Lost.php">Lost</a></li>
-		  <li><a href="Found.php">Found</a></li>
-		  <li><a href="admin_login.php">Admin</a></li>
-		</ul>
-	<img src="white.jpg" height="500" width="1350" style="opacity: 0.8; position: relative; top: -136px; left: 100px;"/>
-	</div>
-		<!--make updates to lost-->
-	<div style="position: relative; bottom: 600px; left: 200px;"> 
+		?>
+			<!--Used to delete items-->
 		<?php 
-		#show lost table
-		admin_show_lost_records($dbc); ?>
-		<br/>
-		<form action ="admin_lost.php" method = "POST">
-			Id #*: <input type="text" name="l_id" value="<?php if (isset($_POST['l_id'])) echo $_POST['l_id'];?>")><br>
-			Status*: <input type="text" name="l_status" value="<?php if (isset($_POST['_status'])) echo $_POST['l_status'];?>")><br>
-			<input type = "submit" >
-		</form>
-	</div>	
-<div style="position: relative; bottom: 600px; left: 200px;"> 
-		<form>
-		<button id="butlost" class="button" formaction="admin.html">Back to task page</button>
-		</form>
-	</div>
-</body>
+			if($_SERVER['REQUEST_METHOD'] == 'GET'){
+				# Connect to MySQL server and the database
+				require( 'includes/connect_limbo_db.php' ) ;
+				# Delete item
+				if(isset($_GET['id']))
+					admin_lost_delete($dbc, $_GET['id']);
+				# Close the connection
+				mysqli_close( $dbc ) ;
+			}
+		?>
+			<!--page layout-->
+		<img src="maristlogo.png" id="maristlogo">
+		<br><br>	
+		<div style="position: relative; left: 0; top: 0;">
+				<!--menu for navigation-->
+			<ul>
+				<li><a href="Home.html">Home</a></li>
+				<li><a href="Lost.php">Lost</a></li>
+				<li><a href="Found.php">Found</a></li>
+				<li><a href="admin_login.php">Admin</a></li>
+			</ul>
+			<img src="white.jpg" height="500" width="1350" style="opacity: 0.8; position: relative; top: -136px; left: 100px;"/>
+		</div>
+			<!--make updates to lost-->
+		<div style="position: relative; bottom: 600px; left: 200px;"> 
+			<?php 
+				#show lost table
+				admin_show_lost_records($dbc); 
+			?>
+			<br>
+			<h1> Update Status of item: </h1>
+			<form action ="admin_lost.php" method = "POST">
+				Id #*: <input type="text" name="l_id" value="<?php if (isset($_POST['l_id'])) echo $_POST['l_id'];?>")><br>
+				Status*: <input type="text" name="l_status" value="<?php if (isset($_POST['l_status'])) echo $_POST['l_status'];?>")><br>
+				<input type = "submit" >
+			</form>
+		</div>	
+			<!-- allow admin to go back to task page-->
+		<div style="position: relative; bottom: 600px; left: 200px;"> 
+			<form>
+				<button id="butlost" class="button" formaction="admin.html">Back to task page</button>
+			</form>
+		</div>
+	</body>
 </html>
